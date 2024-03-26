62f8114dc123c6271a62054c38daab31b0f40127
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-26 15:51:51 +0900

    in

---
+++ Baekjoon2/src/Z2741/Main.java
@@ -0,0 +1,19 @@
+package Z2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		int m = 1;
+
+		while (m <= N) {
+			System.out.println(N++);
+
+		}
+
+	}
+}
